182c6b87dd487127dbc08b575425c944bc6fd7d4
elsayedkewan <107002426+Elsayed23@users.noreply.github.com>
2023-05-14 02:41:00 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,19 @@
-let D1 = document.querySelector(".D1");
-let D2 = document.querySelector(".D2");
-let D3 = document.querySelector(".D3");
+let imgs = document.querySelectorAll(".chats .photos img")
 
 let showIMG = document.querySelector(".showImg img");
 let show = document.querySelector(".showImg");
 let imgClose = document.querySelector(".showImg i");
 
 
-D1.addEventListener("click", () => {
-    let src = D1.getAttribute("src");
-    showIMG.setAttribute("src", src);
-    show.style.display = "block";
-    imgClose.addEventListener("click", () => {
-        show.style.display = "none";
-        showIMG.setAttribute("src", "");
-    })
-})
-D2.addEventListener("click", () => {
-    let src = D2.getAttribute("src");
-    showIMG.setAttribute("src", src);
-    show.style.display = "block";
-    imgClose.addEventListener("click", () => {
-        show.style.display = "none";
-        showIMG.setAttribute("src", "");
-    })
-})
-D3.addEventListener("click", () => {
-    let src = D3.getAttribute("src");
-    showIMG.setAttribute("src", src);
-    show.style.display = "block";
-    imgClose.addEventListener("click", () => {
-        show.style.display = "none";
-        showIMG.setAttribute("src", "");
+
+imgs.forEach((ele) => {
+    ele.addEventListener("click", () => {
+        let src = ele.getAttribute("src");
+        showIMG.setAttribute("src", src);
+        show.style.display = "block";
+        imgClose.addEventListener("click", () => {
+            show.style.display = "none";
+            showIMG.setAttribute("src", "");
+        })
     })
 })
